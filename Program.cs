82a6b6421c6adec3ff6b23d75823ce7e643776de
maxild <mmaxild@gmail.com>
2021-01-12 19:56:20 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SourceCodeAnalysisSpike
+{
+    class Program
+    {
+        // warning IDE0060: Remove unused parameter 'args'
+        static void Main(string[] args)
+        {
+            // warning IDE0059: Unnecessary assignment of a value to 'unused'
+            // warning CS0219: The variable 'unused' is assigned but its value is never used
+            int unused = 0;
+        }
+    }
+}
