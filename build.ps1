c986f779437c1b6cdc4d7d4a6ea13aacbf1ad9e4
Morten Maxild <mmaxild@gmail.com>
2021-01-18 23:58:55 +0100

    Added comment to build.ps1

--- build.ps1
+++ build.ps1
@@ -1,4 +1,5 @@
 #!/usr/bin/env pwsh
 
 dotnet restore ./SourceCodeAnalysisSpike.sln
+# cannot use 'dotnet build' (https://github.com/dotnet/msbuild/issues/3046)
 dotnet msbuild -t:rebuild -p:TreatWarningsAsErrors=true -warnaserror ./SourceCodeAnalysisSpike.sln
